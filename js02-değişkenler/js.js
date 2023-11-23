d7f43a4ef57e4d72f29572981d69104a236888b2
DogancanKaratas <karatasdgncn21@gmail.com>
2023-11-23 14:26:08 +0300

    Day : 1

---
+++ js02-değişkenler/js.js
@@ -0,0 +1,21 @@
+// let ad = "dogancan";
+// let soyad= "karatas";
+
+// console.log(ad);
+// console.log(soyad);
+// console.log(ad + " "+ soyad);
+
+// let müdürAdı = "ahmet";
+// let sınıfBaşkanı = "dogancan"
+
+// console.log("Müdür adı :"+müdürAdı);
+// console.log("sınıf başkanı :"+sınıfBaşkanı);
+// console.log("sunum yapacaklar :"+sınıfBaşkanı, müdürAdı);
+
+const maaş = 7000;
+const zam = 0.1;
+
+let zamlıMaas = maaş + maaş*zam
+
+console.log(zamlıMaas);
+  
---
+++ js03-değişkenTürleri/index.js
@@ -0,0 +1,28 @@
+// !string
+// console.log("Hello world");
+
+// let ogrenciIsmi = "Ahmet Yilmaz";
+// let ogrenciYasi = 25;
+// console.log(typeof ogrenciYasi);
+
+// !number
+// let sayi1= 25;
+// let sayi2 = 20;
+// let toplam = Number(sayi1)+Number(sayi2);
+// let toplam = sayi1.toString()+sayi2.toString();
+// console.log(typeof toplam);
+
+// !boolean
+
+// let sayi1 = 10;
+// let sayi2 = 20;
+// console.log( sayi1 < sayi2);
+// console.log(sayi1 > sayi2);
+// console.log(typeof false);
+// console.log(typeof true);
+
+
+// !undefined
+
+let ogrenciIsmiİ;
+console.log(typeof ogrenciIsmiİ);
